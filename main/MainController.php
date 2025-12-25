5200975f1c8936bb65e3818fb5dfcf1e3a31cbba
flytachi <jasur.rakhmatov03@gmail.com>
2025-12-25 19:06:41 +0500

    update

--- main/MainController.php
+++ main/MainController.php
@@ -2,15 +2,20 @@
 
 namespace Main;
 
-use Flytachi\Winter\Kernel\Stereotype\Output\Response;
 use Flytachi\Winter\Kernel\Stereotype\RestController;
 use Flytachi\Winter\Mapping\Annotation\RequestMapping;
+use Flytachi\Winter\Thread\Signal;
 
 class MainController extends RestController
 {
     #[RequestMapping]
-    public function hello(): Response
+    public function hello(): void
     {
-        return new Response("hello");
+//        $id = TestJob::dispatch();
+//        dd(Signal::interrupt($id));
+//        TestJob::dispatch(['greeting' => 'Hello World!']);
+        $id = TestProcess::dispatch(['greeting' => 'Hello World!']);
+        sleep(5);
+        Signal::interrupt($id);
     }
 }
