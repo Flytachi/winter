084cc7de8d4247ed1f0a2823c0fbc1ff708bcaef
flytachi <jasur.rakhmatov03@gmail.com>
2026-01-05 14:43:57 +0500

    update

--- main/MainController.php
+++ main/MainController.php
@@ -5,7 +5,6 @@ namespace Main;
 use Flytachi\Winter\Kernel\Stereotype\Output\Response;
 use Flytachi\Winter\Kernel\Stereotype\RestController;
 use Flytachi\Winter\Mapping\Annotation\RequestMapping;
-use Flytachi\Winter\Thread\Signal;
 
 class MainController extends RestController
 {
